7af56e0670e587170b32b7cb66b640cd493cdd83
Jorge Izarra <jorgeizarra26@gmail.com>
2024-09-12 12:48:26 -0500

    Laboratorio 5 Completo

---
+++ Laboratorio5/Laboratorio53/Program.cs
@@ -0,0 +1,13 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] frutas = { "manzana", "platano", "naranja" };
+
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
---
+++ Laboratorio5/Laboratorio56/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+        {
+            {"Francia", "Paris"},
+            {"España", "Madrid"},
+            {"Italia", "Roma" }
+        };
+
+        foreach (KeyValuePair<string, string> par in paisesYCapitales)
+        {
+            Console.WriteLine("La capital de " + par.Key + " es " + par.Value + ".");
+        }
+    }
+}
